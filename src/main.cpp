8ac032f38f7bbd70da5de0858afbe0c929194c0c
Seiichi Horie <26223147+suikan4github@users.noreply.github.com>
2024-10-29 21:11:05 +0900

    Fix the problem of the include path.

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
  * @copyright Copyright (c) 2022
  *
  */
-#if __has_include(<hardware/stdlib.h>)
+#if __has_include(<pico/stdlib.h>)
 #include <pico/stdlib.h>
 #else
 #include <stdlib.h>
@@ -26,13 +26,32 @@
 int main() {
   uint8_t address[6];
 
-#if __has_include(<hardware/stdlib.h>)
+#if __has_include(<pico/stdlib.h>)
+
   stdio_init_all();
+  int count = 10;
+  while (count > 0) {
+    sleep_ms(1000);
+    printf("%d\n", count);
+    count--;
+  }
 #endif
 
   RandomMacAddrs::Get(address);
   // count from 1 to num
   for (int i = 0; i < 6; i++) printf("Address[%d] = %02x \n", i, address[i]);
 
+#if __has_include(<pico/stdlib.h>)
+  uint kLed = PICO_DEFAULT_LED_PIN;
+  gpio_init(kLed);
+  gpio_set_dir(kLed, GPIO_OUT);
+  while (true) {
+    gpio_put(kLed, 1);
+    sleep_ms(250);
+    gpio_put(kLed, 0);
+    sleep_ms(250);
+  }
+
+#endif
   return 0;
 }
